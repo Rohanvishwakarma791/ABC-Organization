55dbfb6cf53d3e8072af2d4227777f633bb9f3ac
Rohan Vishwakarma <2018hw86061@wilp.bits-pilani.ac.in>
2022-06-16 14:57:49 +0530

    Updating File3.java

--- Cisco/File3.java
+++ Cisco/File3.java
@@ -9,7 +9,7 @@ class Main {
     System.out.println(first + " " + second);
 
     // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
+    int sub = first - second;
+    System.out.println("The sub is: " + sub);
   }
 }
